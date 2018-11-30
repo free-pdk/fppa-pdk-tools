4ca0d9418f3344fe3bb601a367ecd43943499917
free-pdk <free-pdk@users.noreply.github.com>
2018-11-30 16:32:10 +0100

    show cpu ID for unmapped types, only disassemble used code space and extra region

--- dispdk.c
+++ dispdk.c
@@ -13,17 +13,29 @@ int main( int argc, const char * argv [] )
   }
 
   struct emuCPU cpu;
-  if( (rc=emuCPUloadPDK(&cpu, argv[1])) < 0 ) { 
-    printf("Error reading input file: %d\n",rc); 
-    return -1; 
-  }
-  
+  rc=emuCPUloadPDK(&cpu, argv[1]);
+
   printf("MCU_NAME: %s\n", cpu.hdr.mcu_name);
   printf("OTP_ID: %04X\n", cpu.hdr.otp_id);
   printf("\n");
 
+  if( rc < 0 ) {
+    printf("Error reading input file: %d\n",rc); 
+    return -1; 
+  }
+
   char buf[64] = "-----";
-  for( uint32_t p=0; p < cpu.maxCode; p++ )
+  uint32_t p;
+  for( p=0; p<(cpu.hdr.usable_codesize - cpu.hdr.remain_code_free); p++ )
+  {
+    if( cpu.fnDisassemble) 
+      cpu.fnDisassemble( &cpu, p, buf );
+    printf( "0x%04x:   0x%04x    %s\n", p, cpu.eCode[p], buf );
+  }
+  
+  printf("...\n");
+
+  for( p=cpu.hdr.usable_codesize; p<cpu.hdr.codesize; p++ )
   {
     if( cpu.fnDisassemble) 
       cpu.fnDisassemble( &cpu, p, buf );
