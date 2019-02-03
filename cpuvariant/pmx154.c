b846f6713d52a5dad996630ac1e3dfa0a1527e7c
free-pdk <free-pdk@users.noreply.github.com>
2019-02-03 14:10:47 +0100

    complete IO register naming

--- cpuvariant/pmx154.c
+++ cpuvariant/pmx154.c
@@ -80,14 +80,14 @@ char* pmx154_ioname(struct emuCPU *cpu, uint8_t io, uint8_t bit, char* buffer)
 
     case 0x06: r="T16M"; break;
 
-    case 0x08: r="MISC"; break; //differs from CPU to CPU
-
+    case 0x08: r="MISC"; break;
+    case 0x09: r="TM2B"; break;
     case 0x0A: r="EOSCR"; break;
     case 0x0B: r="IHRCR"; break;
     case 0x0C: r="INTEGS"; break;
     case 0x0D: if(bit<8) sprintf(buffer,"PADIER.%d",bit); else r="PADIER";  break;
     case 0x0E: if(bit<8) sprintf(buffer,"PBDIER.%d",bit); else r="PBDIER";  break;
-
+    case 0x0F: r="MISC2"; break;
     case 0x10: if(bit<8) sprintf(buffer,"PA.%d",bit);  else r="PA";  break;
     case 0x11: if(bit<8) sprintf(buffer,"PAC.%d",bit);  else r="PAC";  break;
     case 0x12: if(bit<8) sprintf(buffer,"PAPH.%d",bit);  else r="PAPH";  break;
@@ -95,9 +95,38 @@ char* pmx154_ioname(struct emuCPU *cpu, uint8_t io, uint8_t bit, char* buffer)
     case 0x14: if(bit<8) sprintf(buffer,"PB.%d",bit);  else  r="PB";  break;
     case 0x15: if(bit<8) sprintf(buffer,"PBC.%d",bit);  else  r="PBC";  break;
     case 0x16: if(bit<8) sprintf(buffer,"PBPH.%d",bit);  else  r="PBPH";  break;
-
+    case 0x17: r="TM2S"; break;
+    case 0x18: r="GPCC"; break;
+    case 0x19: r="GPCS"; break;
     case 0x1A: r="BGTR"; break;
-      case 0x1B: r="MISC_LVR"; break;
+    case 0x1B: r="MISC_LVR"; break;
+    case 0x1C: r="TM2C"; break;
+    case 0x1D: r="TM2CT"; break;
+
+    case 0x20: r="PWMG0C"; break;
+    case 0x21: r="PWMG0S"; break;
+    case 0x22: r="PWMG0DTH"; break;
+    case 0x23: r="PWMG0DTL"; break;
+    case 0x24: r="PWMG0CUBH"; break;
+    case 0x25: r="PWMG0CUBL"; break;
+    case 0x26: r="PWMG1C"; break;
+    case 0x27: r="PWMG1S"; break;
+    case 0x28: r="PWMG1DTH"; break;
+    case 0x29: r="PWMG1DTL"; break;
+    case 0x2A: r="PWMG1CUBH"; break;
+    case 0x2B: r="PWMG1CUBL"; break;
+    case 0x2C: r="PWMG2C"; break;
+    case 0x2D: r="PWMG2S"; break;
+    case 0x2E: r="PWMG2DTH"; break;
+    case 0x2F: r="PWMG2DTL"; break;
+    case 0x30: r="PWMG2CUBH"; break;
+    case 0x31: r="PWMG2CUBL"; break;
+    case 0x32: r="TM3C"; break;
+    case 0x33: r="TM3CT"; break;
+    case 0x34: r="TM3S"; break;
+    case 0x35: r="TM3B"; break;
+
+    case 0x39: r="ILRCR"; break;
 
     default:
       r="?";
